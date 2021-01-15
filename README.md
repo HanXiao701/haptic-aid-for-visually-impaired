2f64bf771aae06e4e4459cf515b5d33da9b2d750
Xiao Han <umiharakawase2010@gmail.com.cn>
2021-01-15 13:30:52 +0800

    Version 1.2

--- README.md
+++ README.md
@@ -1,40 +1,38 @@
-# haptic-aid-for-visually-impaired
-Mobile phone vibration feedback guide interaction for the visually impaired
-One. Demo interface analysis
-From top to bottom are the elements in the Demo
-1. Demo name
-2. Display the direction displayed in the last operation and the input direction
-3. Display the direction expected from the user, that is, the path
-4. Four buttons for inputting direction
-two. Demo usage process
-1. Enter Demo
-2. Enter the direction with the enter button
-3. The device prompts whether the correct direction is input through the intensity of the vibration and the length of the vibration time. If the correct direction is input, it will vibrate for 500ms with an intensity of 100 (the range of vibration intensity is 1-255); In the wrong direction, vibration is performed for 50 ms with an intensity of 200. At the same time, a toast pop-up prompt will prompt the user whether the input is correct. If it is correct, the pop-up content is SUCCESS, if it is wrong, the pop-up content is Failed.
-4. Repeat 2-3
-three. Demo principle
-After the user enters the Demo, the default path is displayed as downward. When the user enters the direction through the keys, a random number from 1-4 will be generated, and the next path will be displayed according to the random number.
-
-four. Current deficiencies
-1. The interaction method is to click the button, and the ideal state should be the user's swipe on the screen.
-2. Since the equipment used for the test is not equipped with a linear motor, the actual effect of the vibration frequency change has not been tested, so it is not known whether there will be any problems in this area.
-3. The Toast pop-up window stays in the user interface for a fixed time and is presented in the form of a queue. Therefore, when the user clicks too quickly, the content of the pop-up window will not be synchronized with the user input.
-
-
 一．	Demo界面解析
-自上至下为Demo中的元素
+ 
 1.	Demo名称
-2.	显示上一次操作所显示的方向与输入的方向
-3.	显示期望用户输入的方向，即路径
-4.	用于输入方向的四个按钮
+2.	显示当前是否振动
+3.	显示当前路径
+4.	用于选择路径的三个按钮
+5.	用于设定当前是否处于有障碍的状态的开关
+6.	触摸区域，用来让用户感受路径的形状
 二．	Demo使用流程
 1.	进入Demo
-2.	通过输入按钮输入方向
-3.	设备通过振动的强度与振动时间的长度提示是否输入了正确的方向，目前若输入了正确的方向，则会进行500ms的振动，强度为100（振动强度范围为1-255）；若输入了错误的方向，则进行50ms的振动，强度为200。同时也将会通过toast弹窗提示的方式提示用户是否输入正确，若正确，则弹窗内容为SUCCESS，若错误，则弹窗内容为Failed。
+2.	通过选择按钮选择路径，通过障碍开关设定是否有障碍
+3.	通过触摸区域感受路径的形状，设备通过振动的强度提示是否存在障碍，若存在障碍则振动强度为200（1-255），若不存在障碍则振动强度为100。
 4.	重复2-3
 三．	Demo原理
-用户进入Demo后默认的路径显示为向下，当用户通过按键输入方向后，则会生成1-4的随机数，并根据该随机数显示下一个路径。
+用户进入Demo后默认的路径为直行，当用户通过触摸区域感受路径形状时，会在用户进入路径区域时振动，离开路径区域是静止。
 
 四．	目前存在的不足
-1.	交互方式为点击按钮，理想状态应为用户对屏幕的划动。
-2.	由于用于测试的设备未配备线性马达，因此并未对振动频率改变的实际效果进行测试因此尚不了解是否会有该方面的问题。
-3.	Toast弹窗在用户界面驻留时间固定且以队列的形式呈现，因此当用户点击过快时弹窗内容将与用户输入不同步。
+目前该Demo仅为对路径感受功能的展示，如果需要进行实验，则需要加入使路径自动生成，而非由用户进行选择的功能。
+
+
+One. Demo interface analysis
+ 
+1. Demo name
+2. Show whether the current vibration
+3. Show current path
+4. Three buttons for path selection
+5. A switch used to set whether the current state of obstacles
+6. Touch area, used to let users feel the shape of the path
+two. Demo usage process
+1. Enter Demo
+2. Select the path through the selection button, and set whether there is an obstacle through the obstacle switch
+3. Feel the shape of the path through the touch area, and the device will prompt whether there is an obstacle through the intensity of the vibration. If there is an obstacle, the vibration intensity is 200 (1-255), and if there is no obstacle, the vibration intensity is 100.
+4. Repeat 2-3
+three. Demo principle
+After the user enters the Demo, the default path is straight. When the user feels the shape of the path through the touch area, it will vibrate when the user enters the path area, and it is still when the user leaves the path area.
+
+four. Current deficiencies
+At present, the Demo is only a demonstration of the path perception function. If you need to experiment, you need to add a function that makes the path automatically generated instead of being selected by the user.
--- app/src/main/java/com/example/vibration/MainActivity.kt
+++ app/src/main/java/com/example/vibration/MainActivity.kt
@@ -16,26 +16,85 @@ import java.util.*
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    var ampli = 1
-    var dura = 1
-    var direction = 2
+    var ampli = 100
+    var dura = 100
+    var direction = 0
 
+    @SuppressLint("ClickableViewAccessibility", "SetTextI18n")
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        binding.buttonUp.setOnClickListener {
-            vibrate(it, 1)
+        binding.buttonUp.setOnTouchListener(View.OnTouchListener { v, event ->
+            //binding.textView.text = event.x.toString() + " " + binding.buttonUp.layoutParams.width.toString() + " " + event.y.toString() + " " + binding.buttonUp.layoutParams.height.toString()
+            binding.textView.text = ampli.toString()
+            if(binding.switch1.isChecked && ampli < 200){
+                ampli = 200
+            } else if(!binding.switch1.isChecked && ampli > 100){
+                ampli = 100
+            }
+            val vib: VibrationEffect = VibrationEffect.createOneShot(dura.toLong(), ampli)
+            when(direction) {
+                0 -> {
+                    //binding.textView.text = "straight"
+                    if (event.x > binding.buttonUp.layoutParams.width / 3 && event.x < binding.buttonUp.layoutParams.width * 2 / 3){
+                        val vibrator = (getSystemService(Service.VIBRATOR_SERVICE) as Vibrator).also {
+                            it.vibrate(vib)
+                        };  //设置手机振动
+                        binding.textView.text = "vibrating"
+                    }else{
+                        binding.textView.text = "static"
+                    }
+                }
+                1 -> {
+                    if (((event.x > binding.buttonUp.layoutParams.width / 3 && event.x < binding.buttonUp.layoutParams.width * 2 / 3)
+                                && event.y > binding.buttonUp.layoutParams.height / 3)
+                        || ((event.y < binding.buttonUp.layoutParams.height / 3)
+                                &&(event.x < binding.buttonUp.layoutParams.width * 2 / 3))){
+                        val vibrator = (getSystemService(Service.VIBRATOR_SERVICE) as Vibrator).also {
+                            it.vibrate(vib)
+                        };  //设置手机振动
+                        binding.textView.text = "vibrating"
+                    }else{
+                        binding.textView.text = "static"
+                    }
+                }
+                else -> {
+                    //binding.textView.text = "right"
+                    if (((event.x > binding.buttonUp.layoutParams.width / 3 && event.x < binding.buttonUp.layoutParams.width * 2 / 3)
+                                && event.y > binding.buttonUp.layoutParams.height / 3)
+                        || ((event.y < binding.buttonUp.layoutParams.height / 3)
+                                &&(event.x > binding.buttonUp.layoutParams.width / 3))){
+                        val vibrator = (getSystemService(Service.VIBRATOR_SERVICE) as Vibrator).also {
+                            it.vibrate(vib)
+                        }  //设置手机振动
+                        binding.textView.text = "vibrating"
+                    }else{
+                        binding.textView.text = "static"
+                    }
+                }
+            }
+            false
+        })
+        binding.straightButton.setOnClickListener {
+            changeRoute(it, 0)
         }
-        binding.buttonDown.setOnClickListener {
-            vibrate(it, 2)
+        binding.leftButton.setOnClickListener {
+            changeRoute(it, 1)
         }
-        binding.buttonLeft.setOnClickListener {
-            vibrate(it, 3)
+        binding.rightButton.setOnClickListener {
+            changeRoute(it, 2)
         }
-        binding.buttonRight.setOnClickListener {
-            vibrate(it, 4)
+    }
+
+    private fun changeRoute(view: View, selectDirection: Int){
+        direction = selectDirection
+        val drawableResource = when (selectDirection) {
+            0 -> R.raw.straight
+            1 -> R.raw.left
+            else -> R.raw.right
         }
+        binding.imageView3.setImageResource(drawableResource)
     }
 
     @SuppressLint("ResourceType")
@@ -99,4 +158,6 @@ class MainActivity : AppCompatActivity() {
         }
         binding.imageView3.setImageResource(drawableResource)
     }
+
+
 }
